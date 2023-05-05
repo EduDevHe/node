7a0c228a006a67403a78d9fe8f23512091d235c5
Eduardo <edu.he.dev@gmail.com>
2023-05-05 18:43:27 -0300

    callback

--- callbacks.js
+++ callbacks.js
@@ -1,25 +1,48 @@
+/**
+ * 0 get a user
+ * 1 get a phone number of a user from your id
+ * 2 get a address of a user from your id
+ */
+
 function getUser(callback) {
   setTimeout(() => {
     return callback(null, {
       id: 1,
       name: "foo bar",
       dateOfBirth: new Date(),
-    })
+    });
   }, 1000);
-  
 }
 
-function getPhoneNumber(idUser) {
-  
+function getPhoneNumber(idUser, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      phone: "000000",
+      dd: "00",
+    });
+  }, 2000);
 }
 
-function getUserAddress(idUser) {
-
+function getUserAddress(idUser, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      endereco: "street a",
+      n: "b",
+    });
+  }, 2000);
 }
 
-function resolveUser(error,user) {
-  console.log("user",user);
+getUser((error, user) => {
+  if (error) return console.log("erro ao obter o usuário");
 
-}
+  console.log(user);
 
-getUser(resolveUser)
+  getPhoneNumber(user.id, (error, phone) => {
+    if (error) return console.log("erro ao obter o telefone do usuário");
+    console.log(phone);
+    getUserAddress(user.id, (error, address) => {
+      if (error) return console.log("erro ao obter o telefone do usuário");
+      console.log(address);
+    });
+  });
+});
